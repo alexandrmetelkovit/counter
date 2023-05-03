9776b7096fc98c5803158fae77fba6041098cc70
Alexandr Metelkov <metel-sanya@mail.ru>
2023-05-03 20:09:01 +0300

    add reset.css and resetbtn

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-let button = document.querySelector('button');
-let counterText = document.querySelector('.counter');
+let button = document.querySelector(".btn__start");
+let button2 = document.querySelector(".btn__reset");
+let counterText = document.querySelector(".counter");
 
 let counter = 0;
 
-button.addEventListener('click', function() {
-    counter = counter + 1;
+button.addEventListener("click", function () {
+  counter = counter + 1;
 
-    counterText.innerText = counter;
-
-
+  counterText.innerText = counter;
 });
 
-
+button2.addEventListener("click", function () {
+  counter = 0;
+  
+  counterText.innerText = counter;
+});
 
 // // DOM - Document Object Model - объектная модель документа
 
