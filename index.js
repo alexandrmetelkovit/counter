b88ebc18b4f149d5cdfef835befb018dc7531b1f
Alexandr Metelkov <metel-sanya@mail.ru>
2023-06-18 22:04:58 +0300

    refactoring code

--- index.js
+++ index.js
@@ -1,58 +1,25 @@
-const counterEl = document.querySelector('[data-find="counterValue"]');
-const addOnePoolButton = document.getElementById("addOnePoolButton");
-const addFivePoolButton = document.getElementById("addFivePoolButton");
-const resetButton = document.getElementById("resetButton");
-
-console.log(addOnePoolButton.dataset);
+const counterNum = document.querySelector(".js-counter__value");
+const addOneDayButton = document.querySelector(".js-counter__addOneDayButton");
+const addOTwoDayButton = document.querySelector(".js-counter__addTwoDayButton");
+const resetButton = document.querySelector(".js-counter__resetButton");
 
 const COUNTER_INITIAL_VALUE = 0;
 
 let counter = COUNTER_INITIAL_VALUE;
-counterEl.innerText = counter;
 
-addOnePoolButton.addEventListener("click", function () {
-  counter = counter + parseInt(addOnePoolButton.dataset.pools);
-  counterEl.innerText = counter;
+counterNum.innerText = counter;
+
+addOneDayButton.addEventListener("click", function () {
+  counter++;
+  counterNum.innerText = counter;
 });
-addFivePoolButton.addEventListener("click", function () {
-  counter = counter + parseInt(addFivePoolButton.dataset.pools);
-  counterEl.innerText = counter;
+
+addOTwoDayButton.addEventListener("click", function () {
+  counter = counter + 2;
+  counterNum.innerText = counter;
 });
 
 resetButton.addEventListener("click", function () {
   counter = COUNTER_INITIAL_VALUE;
-  counterEl.innerText = counter;
+  counterNum.innerText = counter;
 });
-
-
-
-// let button = document.querySelector('button'); //обрабатываем нажатие кнопки
-// let counterText = document.querySelector('.counter__title'); // связь с эл на стр
-
-// let counter = 0; // создаем переменную / данные
-
-// button.addEventListener('click', function() { // обращаюсь к кнопке (button) => давай по этой кнопке обрабатывать событие (addEventListener)  добавь отработку действия => далее указываю, что я хочу обрабатывать по этой кнопке ('click')
-//   counter++;
-
-//   counterText.innerText = counter;
-// });
-
-
-// // console.log(document); // DOM - document object model
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
